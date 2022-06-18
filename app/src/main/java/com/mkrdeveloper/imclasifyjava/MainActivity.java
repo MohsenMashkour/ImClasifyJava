9beb5c0613caae0668b25f9c5fc0226c0b3b8028
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-06-18 18:32:43 +0200

    first commit

---
+++ app/src/main/java/com/mkrdeveloper/imclasifyjava/MainActivity.java
@@ -0,0 +1,134 @@
+package com.mkrdeveloper.imclasifyjava;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.mkrdeveloper.imclasifyjava.ml.MoModel;
+import com.mkrdeveloper.imclasifyjava.ml.MoModel2;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.schema.Model;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView result, confidence;
+    ImageView imageView;
+    Button picture;
+    int imageSize = 224;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        result = findViewById(R.id.result);
+        confidence = findViewById(R.id.confidence);
+        imageView = findViewById(R.id.imageView);
+        picture = findViewById(R.id.button);
+
+        picture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Launch camera if we have permission
+                if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, 1);
+                } else {
+                    //Request camera permission if we don't have it.
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent intent) {
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+
+            Bitmap bitmap = (Bitmap) intent.getExtras().get("data");
+
+            int dimentions = Math.min(bitmap.getWidth(), bitmap.getHeight());
+            bitmap = ThumbnailUtils.extractThumbnail(bitmap, dimentions, dimentions);
+            imageView.setImageBitmap(bitmap);
+            bitmap = Bitmap.createScaledBitmap(bitmap, imageSize,imageSize,false);
+            clasifyImage(bitmap);
+        }
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+    @SuppressLint("DefaultLocale")
+    private void clasifyImage(Bitmap image) {
+        try {
+            MoModel2 model = MoModel2.newInstance(this);
+
+            // Creates inputs for reference.
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * imageSize * imageSize * 3);
+            byteBuffer.order(ByteOrder.nativeOrder());
+
+            int [] intValues = new int[imageSize * imageSize];
+            image.getPixels(intValues, 0, image.getWidth(), 0, 0, image.getWidth(), image.getHeight());
+
+            // iterate over pixels and extract R, G, and B values. Add to bytebuffer.
+            int pixel = 0;
+            for(int i = 0; i < imageSize; i++){
+                for(int j = 0; j < imageSize; j++){
+                    int val = intValues[pixel++]; // RGB
+                    byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                    byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+                }
+            }
+            inputFeature0.loadBuffer(byteBuffer);
+
+            // Runs model inference and gets result.
+            MoModel2.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+
+            float[] confidences = outputFeature0.getFloatArray();
+            // find the index of the class with the biggest confidence.
+            int maxPos = 0;
+            float maxConfidence = 0;
+            for(int i = 0; i < confidences.length; i++){
+                if(confidences[i] > maxConfidence){
+                    maxConfidence = confidences[i];
+                    maxPos = i;
+                }
+            }
+            String[] classes = {"mouse", "card", "charger","20zl","10zl"};
+            result.setText(classes[maxPos]);
+
+            StringBuilder s = new StringBuilder();
+            for(int i = 0; i < classes.length; i++){
+                s.append(String.format("%s: %.1f%%\n", classes[i], confidences[i] * 100));
+            }
+            confidence.setText(s.toString());
+
+            // Releases model resources if no longer used.
+            model.close();
+        } catch (IOException e) {
+            // TODO Handle the exception
+        }
+
+    }
+}
